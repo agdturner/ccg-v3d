54ab53f48693cceecd80e249a9b55250c5b54e06
Andy Turner <a.g.d.turner@leeds.ac.uk>
2020-12-15 11:02:15 +0000

    Javadoc updates.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,42 @@
+/*
+ * Copyright 2020 Andy Turner, University of Leeds.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Provides utility for 3D spatial geometry.
+ */
 module uk.ac.leeds.ccg.vector3D {
+
     /**
-     * Requirements.
+     * The big-math library is mostly used for representing and computing with
+     * rational numbers as {@link ch.obermuhlner.math.big.BigRational}. It is
+     * also for some functions that work on {@link java.math.BigDecimal}.
      */
     requires ch.obermuhlner.math.big;
+
+    /**
+     * The agdt-java-generic library is used for generic functionality.
+     */
     requires transitive uk.ac.leeds.ccg.generic;
+
+    /**
+     * The agdt-java-math library is used for some computing with numbers stored
+     * as {@link java.math.BigDecimal}.
+     */
     requires transitive uk.ac.leeds.ccg.math;
-    
+
     /**
      * Exports.
      */
